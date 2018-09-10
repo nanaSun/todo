ef4717edd309a94d91326c5ccd8857b70a82a8da
nanaSun <1128yeye@sina.com>
2018-09-10 15:24:56 +0800

    v0.2.1

--- src/Components/Operator.js
+++ src/Components/Operator.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import '../styles/Operator.css';
 
 /**
  *  操作栏
  */
 
-class Operator extends Component {
-  render() {
-    return (
-      <div className="OperatorBar">
-        <button onClick={this.props.toggleComplete}>{this.props.showComplete?"Show All":"Hide Complete"}</button>
-        <button>Add Todo</button>
-      </div>
-    );
-  }
+function Operator(props){
+  return (
+    <div className="OperatorBar">
+      <button onClick={props.toggleComplete}>{props.showComplete?"Show All":"Hide Complete"}</button>
+      <button>Add Todo</button>
+    </div>
+  );
 }
 
 export default Operator;
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,38 +1,21 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import '../styles/Todo.css';
-
 /**
  *  每一列的todo内容
  *  需要Todolist传入props
  */
 
-class Todo extends Component {
-  state={}
-  constructor(props){
-    super(props)
-    this.state.status=this.props.status
-  }
-  toggleState(){
-    console.log(this.state)
-    this.setState({
-      status:this.state.status===0?1:0
-    })
-  }
-  componentWillReceiveProps(){
-    console.log(this.props)
-  }
-  render() {
-      return (
-        <div className={this.props.status===1?"Todo completed":"Todo"}>
-          <div className="TodoOperate" onClick={()=>this.props.toggleState({
-            id:this.props.id,
-            content:this.props.content,
-            status:this.props.status===0?1:0
-          })}></div> 
-          <p>{this.props.content}</p>
-        </div>
-      );
-  }
+function Todo(props){
+  return (
+    <div className={props.status===1?"Todo completed":"Todo"}>
+      <div className="TodoOperate" onClick={()=>props.toggleState({
+        id:props.id,
+        content:props.content,
+        status:props.status===0?1:0
+      })}></div> 
+      <p>{props.content}</p>
+    </div>
+  );
 }
 
 export default Todo;
